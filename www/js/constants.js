3ca8afea47d464c4c598233c781e6115018cc8f1
Palotec <palotec@SuperMan.local>
2016-05-24 16:41:30 -0300

    configuração para testar no nagegador

--- www/js/constants.js
+++ www/js/constants.js
@@ -6,5 +6,8 @@ angular.module('starter')
 
 .constant('API_ENDPOINT', {
   url: 'http://localhost:8100/api'
+
+  //Utilizar o link do servidor real para os aplicativos
+  //url: 'http://64.137.249.103:8080/api'
   //  For a simulator use: url: 'http://127.0.0.1:8080/api'
 });
